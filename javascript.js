5a2422a7048fad8ce3ebdf1712a07f97392419fd
paulvu2023 <118864214+paulvu2023@users.noreply.github.com>
2023-07-04 14:38:39 -0700

    Add user and computer score

--- javascript.js
+++ javascript.js
@@ -34,8 +34,20 @@ const buttons = document.querySelectorAll('button');
 buttons.forEach((button) => {
     button.addEventListener('click', () => {
         const msg = document.querySelector('div#round');
+        const userScoreSelector = document.querySelector('#userScore');
+        const compScoreSelector = document.querySelector('#compScore');
         let winner = playRound(button.id, getComputerChoice());
+        let userScore = parseInt(userScoreSelector.textContent);
+        let compScore = parseInt(compScoreSelector.textContent);
         msg.textContent = winner;
+        // check string to see if user won
+        if (winner.slice(0, 5) == 'You w') {
+            userScore += 1;
+        } else if (winner.slice(0, 5) == 'You l') {
+            compScore += 1;
+        }
+        userScoreSelector.textContent = userScore;
+        compScoreSelector.textContent = compScore;
     });
 });
 
