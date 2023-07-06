3afa1872b09b80c5a7f643de8fbba03246d8f823
paulvu2023 <118864214+paulvu2023@users.noreply.github.com>
2023-07-06 15:58:23 -0700

    Change fight scene when button clicked

--- javascript.js
+++ javascript.js
@@ -12,6 +12,7 @@ function getComputerChoice(){
 function playRound(playerSelection, computerSelection){
     playerSelection = playerSelection.toLowerCase();
     playerSelection = playerSelection[0].toUpperCase() + playerSelection.slice(1);
+    changeFightImage(playerSelection, computerSelection);
     if (playerSelection === computerSelection){
         return("Tie. You're lucky.");
     }
@@ -25,8 +26,29 @@ function playRound(playerSelection, computerSelection){
             || computerSelection === "Kick" && playerSelection === "Jab")
     {
         return `You lose! ${computerSelection} beats ${playerSelection}!`;
-    } else {
-        return(`You lose! ${computerSelection} beats ${playerSelection}!`);
+    }
+}
+
+function changeFightImage (playerSelection, computerSelection){
+    const fightScene = document.querySelector('.fightScene');
+    if (playerSelection === "Jab" && computerSelection === "Jab") {
+        fightScene.src = "images/jabjab.png";
+    } else if (playerSelection === "Kick" && computerSelection === "Kick") {
+        fightScene.src = "images/kickkick.png";
+    } else if (playerSelection === "Parry" && computerSelection === "Parry") {
+        fightScene.src = "images/parryparry.png";
+    } else if (playerSelection === "Jab" && computerSelection === "Parry") {
+        fightScene.src = "images/jabparry.png";
+    } else if (playerSelection === "Parry" && computerSelection === "Kick") {
+        fightScene.src = "images/parrykick.png";
+    } else if (playerSelection === "Kick" && computerSelection === "Jab") {
+        fightScene.src = "images/kickjab.png";
+    } else if (computerSelection === "Jab" && playerSelection === "Parry") {
+        fightScene.src = "images/parryjab.png";
+    } else if (computerSelection === "Parry" && playerSelection === "Kick") {
+        fightScene.src = "images/kickparry.png";
+    } else if (computerSelection === "Kick" && playerSelection === "Jab") {
+        fightScene.src = "images/jabkick.png";
     }
 }
 
@@ -40,6 +62,12 @@ buttons.forEach((button) => {
         let winner = playRound(button.id, getComputerChoice());
         let userScore = parseInt(userScoreSelector.textContent.slice(12));
         let compScore = parseInt(compScoreSelector.textContent.slice(13));
+        
+        if (userScore === 0 && compScore === 0) {
+            fightScene = document.querySelector('.fightScene');
+            fightScene.src = "images/defaultposition.png";
+        }
+
         if (userScore < 5 && compScore < 5) {
             roundWinner.textContent = winner;
             // check string to see if user won or if computer won and change score
